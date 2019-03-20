3875b4f0e94a3a915962db650530fc7a980ca393
ghwlchlaks <ghwlchlaks@naver.com>
2019-03-21 00:19:31 +0900

    datepicker 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import Stepper from 'react-stepper-horizontal';
-import { Button, Form, FormGroup, Label, Input, Container } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Container, Jumbotron } from 'reactstrap';
+import DatePicker from 'react-datepicker';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      isStart: true,
       currentStep : 0,
       date: ''
     }
-
+    this.startClickHandler = this.startClickHandler.bind(this)
     this.nextStepClickHandler = this.nextStepClickHandler.bind(this);
     this.changeDate = this.changeDate.bind(this);
   }
@@ -25,8 +27,12 @@ class App extends Component {
           alert('틀렸어요 ㅡㅡ')
         }
         return;
-      default:
-        window.location.reload();
+      case 1:
+        this.nextStep()
+        return;
+      case 2:
+        this.nextStep();
+        return;
     }
   }
 
@@ -52,16 +58,40 @@ class App extends Component {
         return <Step1 changeDate={this.changeDate} />;
       case 1:
         return <Step2 />;
+      case 2: 
+        return <Step3 />;
       default: 
         return null;
     }
   }
 
+  startClickHandler() {
+    this.setState({
+      isStart: false
+    })
+  }
+
   render() {
+    if (this.state.isStart) {
+      return (
+      <div>
+        <Jumbotron>
+          <h1 className="display-3">Hello, world!</h1>
+          <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr className="my-2" />
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <p className="lead">
+            <Button onClick={this.startClickHandler} color="primary">Learn More</Button>
+          </p>
+        </Jumbotron>
+      </div>
+      )
+    }
+
     return (
       <div>
         <Container>
-          <Stepper steps={ [{title: '본인 인증'}, {title: 'Step Two'}, {title: 'Step Three'}, {title: 'Step Four'}] } activeStep={ this.state.currentStep } />
+          <Stepper steps={ [{title: '본인 인증'}, {title: '이용가능한 날짜'}, {title: '스타일'}, {title: '최종확인'}] } activeStep={ this.state.currentStep } />
             <div id='contents'>
               {this.renderSteps()}
             </div>
@@ -75,18 +105,44 @@ class App extends Component {
 class Step1 extends Component {
   render(){
     return (
-    <Form>
       <FormGroup row>
         <Label for="exampleEmail">날짜</Label>
         <Input type="text" name="date" id="date" onChange={this.props.changeDate} placeholder="yyyymmdd 형태로 적으세요" />
         <Label for="exampleEmail">예) 1994년 11월 19일이라면 : 19941119</Label>
       </FormGroup>
-    </Form>
     )
   }
 }
 
 class Step2 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state ={
+      startDate: new Date()
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <DatePicker
+          selected={this.state.startDate}
+          onChange={this.handleChange}
+        />
+      </div>
+    )
+  }
+}
+
+class Step3 extends Component {
   render() {
     return (
       <Form>
@@ -103,14 +159,6 @@ class Step2 extends Component {
           </Label>
         </FormGroup>
       </Form>
-    )
-  }
-}
-
-class Step3 extends Component {
-  render() {
-    return (
-      <div>step3</div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
